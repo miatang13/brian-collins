c220896de5da4fa2d262b7871c6702df201284b8
miatang <miatang13@gmail.com>
2020-05-15 18:40:33 -0400

    z

--- server.js
+++ server.js
@@ -1,42 +1,23 @@
-var express = require('express');
-var app = express();
-var history = require('connect-history-api-fallback');
+const express = require('express')
+const path = require('path')
+const history = require('connect-history-api-fallback')
+// ^ middleware to redirect all URLs to index.html
 
-app.use(history({
-// verbose: true
-}));
+const app = express()
+const staticFileMiddleware = express.static(path.join(__dirname, '/dist'))
 
-app.use(express.static(__dirname));
+app.use(staticFileMiddleware)
+app.use(history())
+app.use(staticFileMiddleware)
+// ^ `app.use(staticFileMiddleware)` is included twice as per https://github.com/bripkens/connect-history-api-fallback/blob/master/examples/static-files-and-index-rewrite/README.md#configuring-the-middleware
 
-app.set('port', (process.env.PORT || 8080));
+app.get('/', function (req, res) {
+  res.render(path.join(__dirname + '/index.html'))
+})
 
-// app.listen(app.get('port'), () => {
-// console.log(ENV: ${process.env.NODE_ENV});
-// console.log(Derp is running at localhost: ${app.get('port')});
-// });
-
-
-
-// const express = require('express')
-// const path = require('path')
-// const history = require('connect-history-api-fallback')
-// // ^ middleware to redirect all URLs to index.html
-
-// const app = express()
-// const staticFileMiddleware = express.static(path.join(__dirname, '/dist'))
-
-// app.use(staticFileMiddleware)
-// app.use(history())
-// app.use(staticFileMiddleware)
-// // ^ `app.use(staticFileMiddleware)` is included twice as per https://github.com/bripkens/connect-history-api-fallback/blob/master/examples/static-files-and-index-rewrite/README.md#configuring-the-middleware
-
-// app.get('/', function (req, res) {
-//   res.render(path.join(__dirname + '/index.html'))
-// })
-
-// app.listen(5000, function () {
-//   console.log( 'Express serving on 5000!' )
-// })
+app.listen(5000, function () {
+  console.log( 'Express serving on 5000!' )
+})
 
 
 
