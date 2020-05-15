d20a766e41d4bcf16cede304e8c31b689f14fa39
miatang <miatang13@gmail.com>
2020-05-15 18:19:13 -0400

    z

--- server.js
+++ server.js
@@ -1,23 +1,48 @@
-const express = require('express');
-const path = require('path');
-const history = require('connect-history-api-fallback');
+const express = require('express')
+const path = require('path')
+const history = require('connect-history-api-fallback')
+// ^ middleware to redirect all URLs to index.html
 
-const app = express();
+const app = express()
+const staticFileMiddleware = express.static(path.join(__dirname))
 
-const staticFileMiddleware = express.static(path.join(__dirname + '/dist'));
-
-app.use(staticFileMiddleware);
-app.use(history({
-  disableDotRule: true,
-  verbose: true
-}));
-app.use(staticFileMiddleware);
+app.use(staticFileMiddleware)
+app.use(history())
+app.use(staticFileMiddleware)
+// ^ `app.use(staticFileMiddleware)` is included twice as per https://github.com/bripkens/connect-history-api-fallback/blob/master/examples/static-files-and-index-rewrite/README.md#configuring-the-middleware
 
 app.get('/', function (req, res) {
-  res.render(path.join(__dirname + '/dist/index.html'));
-});
+  res.render(path.join(__dirname + '/index.html'))
+})
 
-var server = app.listen(process.env.PORT || 8080, function () {
-  var port = server.address().port;
-  console.log("App now running on port", port);
-});
+app.listen(5000, function () {
+  console.log( 'Express serving on 5000!' )
+})
+
+
+
+
+
+// const express = require('express');
+// const path = require('path');
+// const history = require('connect-history-api-fallback');
+
+// const app = express();
+
+// const staticFileMiddleware = express.static(path.join(__dirname + '/dist'));
+
+// app.use(staticFileMiddleware);
+// app.use(history({
+//   disableDotRule: true,
+//   verbose: true
+// }));
+// app.use(staticFileMiddleware);
+
+// app.get('/', function (req, res) {
+//   res.render(path.join(__dirname + '/dist/index.html'));
+// });
+
+// var server = app.listen(process.env.PORT || 8080, function () {
+//   var port = server.address().port;
+//   console.log("App now running on port", port);
+// });
