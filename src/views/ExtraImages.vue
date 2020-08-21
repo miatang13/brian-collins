c2ac81c1b6223d8a53e0a36e55f34af75de425df
miatang <miatang13@gmail.com>
2020-08-21 16:50:01 -0700

    z

--- src/views/ExtraImages.vue
+++ src/views/ExtraImages.vue
@@ -2,6 +2,7 @@
  <div>
      <img src = '@/assets/photos/mywork/booklet/booklet.gif' width = "100%">
      <img src = '@/assets/photos/mywork/extra/where.png' width = "100%">
+     <img src = '@/assets/photos/mywork/extra/skyglow.gif' width = "100%">
  </div>
 </template>
 
